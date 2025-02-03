54e4d2e7881894979f0552dcd3c107d312601b94
Stoyan Penev <globalfunc@gmail.com>
2025-02-03 17:25:49 +0200

    Add translations and remove edit group info action.

--- resources/views/components/chat/header.blade.php
+++ resources/views/components/chat/header.blade.php
@@ -31,7 +31,7 @@
                         class="h-8 w-8 lg:w-10 lg:h-10 " />
                     <h6 class="font-bold text-base text-gray-800 dark:text-white w-full truncate">
                         {{ $group ? $group?->name : $receiver?->display_name }} @if ($conversation->isSelfConversation())
-                            (You)
+                            ({{__('You')}})
                         @endif
                     </h6>
                 </div>
@@ -44,44 +44,27 @@
                 <x-wirechat::dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex px-0 text-gray-700 dark:text-gray-400">
-                            {{-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                                class="w-6 h-6 text-gray-600 dark:text-white/90 stroke-[1.4] dark:stroke-[1.3]">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                            </svg> --}}
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                 stroke-width="1.9" stroke="currentColor" class="size-6 w-7 h-7">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
                             </svg>
-
                         </button>
                     </x-slot>
                     <x-slot name="content">
 
-                        <button
-                            wire:click="$dispatch('openChatDrawer', {component: 'info',arguments: { conversation: {{ $conversation->id }} }})"
-                            class="w-full text-start">
-
-                            <x-wirechat::dropdown-link>
-                                {{ $conversation->isGroup() ? 'Group' : 'Chat' }} Info
-                            </x-wirechat::dropdown-link>
-
-                        </button>
-
-
                         <x-wirechat::dropdown-link href='{{ route(WireChat::indexRouteName()) }}'>
-                            Close Chat
+                            {{__('Close Chat')}}
                         </x-wirechat::dropdown-link>
 
 
                     {{-- Only show delete and clear if conversation is NOT group --}}
                     @if (!$conversation->isGroup())
                     <button class="w-full" wire:click="clearConversation"
-                        wire:confirm="Are you sure you want to clear this Chat History ?">
+                        wire:confirm="{{__('Are you sure you want to clear this Chat History?')}}">
 
-                        <x-wirechat::dropdown-link >
-                            Clear Chat History
+                        <x-wirechat::dropdown-link>
+                            {{__('Clear Chat History')}}
                         </x-wirechat::dropdown-link>
                     </button>
 
@@ -90,14 +73,14 @@
                         class="w-full text-start">
 
                         <x-wirechat::dropdown-link class="text-red-500 dark:text-red-500">
-                            Delete Chat
+                            {{__('Delete Chat')}}
                         </x-wirechat::dropdown-link>
 
                     </button>
                     @endif
 
 
-                    @if ($conversation->isGroup() && !auth()->user()->isOwnerOf($conversation))
+                    @if (false && $conversation->isGroup() && !auth()->user()->isOwnerOf($conversation))
                             <button wire:click="exitConversation" wire:confirm="Are you sure want to exit Group?"
                                 class="w-full text-start ">
 
