d01238c1fd4d7920f584c72424d9b74199caf89b
Stoyan Penev <globalfunc@gmail.com>
2025-02-04 13:00:32 +0200

    Remove download button if message is not an attachment.

--- resources/views/components/chat/body.blade.php
+++ resources/views/components/chat/body.blade.php
@@ -174,11 +174,13 @@
                                                 d="M5.921 11.9 1.353 8.62a.72.72 0 0 1 0-1.238L5.921 4.1A.716.716 0 0 1 7 4.719V6c1.5 0 6 0 7 8-2.5-4.5-7-4-7-4v1.281c0 .56-.606.898-1.079.62z" />
                                         </svg>
                                     </button>
+                                    @if ($message->hasAttachment())
                                     <x-filament::icon-button
                                             wire:click="downloadAttachment('{{$message->id}}')"
                                             icon="heroicon-o-arrow-down-tray"
                                             class="invisible group-hover:visible hover:scale-110 transition-transform"
                                             color="currentColor"/>
+                                    @endif
                                     {{-- Dropdown actions button --}}
                                     <x-wirechat::dropdown class="w-40" align="{{ $belongsToAuth ? 'right' : 'left' }}"
                                         width="48">
